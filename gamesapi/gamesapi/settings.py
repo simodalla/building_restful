494dd92e5fe7ba5c83d43f67925fc53768ab31d4
Simone Dalla <simodalla@gmail.com>
2016-11-11 18:11:27 +0100

    start ch2 managing serializations..

--- gamesapi/gamesapi/settings.py
+++ gamesapi/gamesapi/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'gamesapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'games',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1'
     }
 }
 
